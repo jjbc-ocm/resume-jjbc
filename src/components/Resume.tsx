82b48c7326fa9f852bd37b1b905ef77ff6f7905d
jjbc-ocm <104966216+jjbc-ocm@users.noreply.github.com>
2025-07-19 14:36:20 +0800

    Update Resume.tsx

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -148,7 +148,7 @@ export default function Resume() {
                     <Typography variant="h6" color="#d1d5db" sx={{ fontWeight: 400, mb: 3 }}>
                         Professional Experience & Skills
                     </Typography>
-                    <Button
+                    {/* <Button
                         variant="contained"
                         startIcon={<Download />}
                         onClick={handleOpenPrintWindow}
@@ -173,7 +173,7 @@ export default function Resume() {
                         }}
                     >
                         Open in New Window
-                    </Button>
+                    </Button> */}
                     <Button
                         variant="contained"
                         startIcon={<Download />}
